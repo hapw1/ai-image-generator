5cf77a7befdfcc1ba5e433fedef07c72755ff937
hapw1 <hapw1.developer@gmail.com>
2024-02-27 14:20:49 +0000

    Revert "commit to revert"
    This reverts commit ef43edfcddd2ee9ede443c018426bf2c62050534.

--- ai-image-generator/ai-image-generator/ai_image_generatorApp.swift
+++ ai-image-generator/ai-image-generator/ai_image_generatorApp.swift
@@ -2,16 +2,20 @@
 //  ai_image_generatorApp.swift
 //  ai-image-generator
 //
-//  Created by Harry Witcomb on 27/02/2024.
+//  Created by Harry Witcomb on 22/02/2024.
 //
 
 import SwiftUI
+import SwiftOpenAI
+
 
 @main
 struct ai_image_generatorApp: App {
+
     var body: some Scene {
+
         WindowGroup {
-            ContentView()
+            HomeView(service: OpenAIServiceFactory.service(apiKey: "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"))
         }
     }
 }
